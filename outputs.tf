71b954fe5730adb6f402ea48a237693b0a37c5ef
Loren Gordon <lorengordon@users.noreply.github.com>
2023-12-26 15:35:49 -0800

    Refactors config module for modern aws/terraform and all resource options

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,3 @@
-output "config_iam_role_arn" {
-  description = "The Amazon Resource Name (ARN) of the config service role"
-  value       = local.create_iam_role ? aws_iam_role.this[0].arn : ""
-}
-
-output "config_iam_role_name" {
-  description = "The name of the config service role"
-  value       = local.create_iam_role ? aws_iam_role.this[0].name : ""
-}
-
 output "config_recorder_id" {
   description = "The name of the AWS Config recorder"
   value       = aws_config_configuration_recorder.this.id
@@ -16,9 +6,4 @@ output "config_recorder_id" {
 output "config_delivery_channel_id" {
   description = "The name of the AWS Config delivery channel"
   value       = aws_config_delivery_channel.this.id
-}
-
-output "config_sns_topic_arn" {
-  description = "The Amazon Resource Name (ARN) of the config SNS topic"
-  value       = aws_sns_topic.this.arn
 }
