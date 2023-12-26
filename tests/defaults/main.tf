e46dad3d65515dcac9048264aa064f5b8e28810d
Loren Gordon <lorengordon@users.noreply.github.com>
2023-12-26 15:36:14 -0800

    Updates test for refactored module

--- tests/defaults/main.tf
+++ tests/defaults/main.tf
@@ -3,19 +3,7 @@ module "config" {
 
   config = {
     configuration_recorder = {
-      name            = "tardigrade-config-${random_string.this.result}"
-      recording_group = {
-        all_supported = false
-
-        recording_strategy = {
-          use_only = "INCLUSION_BY_RESOURCE_TYPES"
-        }
-
-        resource_types = [
-          "AWS::EC2::Instance",
-          "AWS::CloudTrail::Trail",
-        ]
-      }
+      name = "tardigrade-config-${random_string.this.result}"
     }
 
     delivery_channel = {
